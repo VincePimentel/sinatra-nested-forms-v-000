56c1eac5c4dddbce3aeb570fdaad8701717f38e7
Vince Andrew Pimentel <contact@vincep.me>
2019-01-09 02:10:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,5 +7,15 @@ module FormsLab
       erb :root
     end
 
+    get "/new" do
+      erb :new
+    end
+
+    post "/pirates" do
+      @pirate = Pirate.new(params[:pirate])
+
+      erb :show
+    end
+
   end
 end
