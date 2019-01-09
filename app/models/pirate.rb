2d91c5d281d347ac9d039d725d937b3a13e29030
Vince Andrew Pimentel <contact@vincep.me>
2019-01-09 01:47:09 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,11 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  PIRATES = Array.new
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+  end
 end
