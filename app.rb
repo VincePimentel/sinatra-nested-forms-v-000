97efa4bbda205d9a17e059ad89d75689deb22f0d
Vince Andrew Pimentel <contact@vincep.me>
2019-01-09 02:47:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,15 +12,15 @@ module FormsLab
     end
 
     post "/pirates" do
-      # @pirate = Pirate.new(params[:pirate])
-      #
-      # params[:pirate][:ships].each do |ship|
-      #   Ship.new(ship)
-      # end
-      #
-      # @ships = Ship.all
+      @pirate = Pirate.new(params[:pirate])
 
-      # erb :show
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship)
+      end
+
+      @ships = Ship.all
+
+      erb :"pirates/show"
     end
 
   end
