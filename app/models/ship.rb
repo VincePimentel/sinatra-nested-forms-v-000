2d4be67f5f1cb5404f41e5a3eb7dcf8d1debe2a6
Vince Andrew Pimentel <contact@vincep.me>
2019-01-09 01:51:10 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,11 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+  SHIPS = Array.new
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+  end
 end
